9719c0512bbdd3bfa226b4e8de5256290906ac21
12024 <gisellevidalulloa@gmail.com>
2021-06-09 12:30:49 -0400

    errores arreglados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharactersComponent } from './characters/characters.component';
-import { DetailComponent } from './series/detail/detail.component';
 import { SeriesComponent } from './series/series.component';
 import { DetailComponent } from './series/serie/detail/detail.component';
 
@@ -15,20 +14,9 @@ const routes: Routes = [
   {
     path: 'series', component: SeriesComponent
   },
-<<<<<<< HEAD
   {
     path: 'series/:id', component: DetailComponent
-  }
-
-
-
-||||||| 133ca34
-
-=======
-  {
-    path: 'series/:id', component: DetailComponent
-  }
->>>>>>> 7b5b598f571c6c416f361a25fe1c7d022279ab05
+  },
 ];
 
 @NgModule({
